215d8df130d396a171f71b38fcc215b35458e7f3
Nolanlemahn <nolan@projectexist.net>
2015-09-06 17:23:28 -0700

    Merge Part 2

---
+++ GAM200_Project/GAM200_Project/engineGraphics/Graphics.h
@@ -0,0 +1,40 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#ifdef _WIN32
+	#pragma comment(lib, "graphicsLibsWin/glew32s.lib")
+	#pragma comment(lib, "graphicsLibsWin/SOIL.lib")
+	#pragma comment(lib, "./OpenGL32.Lib" )
+#endif
+
+//Core Graphics Functionality 
+#define GLEW_STATIC
+#include "glew/glew.h"
+#include "glew/wglew.h"
+
+//Window Creation
+#include <Windows.h>
+
+//Image Loading 
+#include "SOIL/SOIL.h"
+
+//Math
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+//Printing out stuff
+#include<iostream>
+
+#include <vector>
+
+#define WINDOWWIDTH 960
+#define WINDOWHEIGHT 540
+
+void setupPixelFormatDescriptor(HDC handleDeviceContext);
+HWND createWindow(HINSTANCE instance);
+
+//This function prototype is in here so createwindow can see it 
+LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+
+#endif
