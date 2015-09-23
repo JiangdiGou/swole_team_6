2d437b732a45a18b04223d6629088fc9c96b2e2e
Qbz23 <conorlavelleqbz@gmail.com>
2015-09-23 14:28:38 -0700

    Took the Instanced testing out of the GFX main and made sure the non-instanced drawing was still working properly Added a new visual studio project for me to work on more complicated graphics stuff in, like instancing.

--- GAM200_Project/GAM200_Project/engineGraphics/GFXMain.cpp
+++ GAM200_Project/GAM200_Project/engineGraphics/GFXMain.cpp
@@ -28,107 +28,108 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE hPreviousInstance, LPSTR comman
 int falseMain1(HINSTANCE instance, HINSTANCE hPreviousInstance, LPSTR command_line, int show)
 #endif
 {
-    //Openas a console for debugging and testing 
-    AllocConsole();
-    freopen("CONOUT$", "w", stdout);
+  //Openas a console for debugging and testing 
+  AllocConsole();
+  freopen("CONOUT$", "w", stdout);
 
+  //Stores the window being created
+  HWND window; 
+  //Stores windows messages 
+  MSG msg;
 
+  //Creates a window, saves it in hwnd, and shows on screen
+  window = createWindow(instance);
+  ShowWindow(window, show);
 
-    //Stores the window being created
-    HWND window; 
-    //Stores windows messages 
-    MSG msg;
+  //Creates the core shader and camera 
+  Shader basicShader = Shader("resources/VertexShader.txt", "resources/FragmentShader.txt");
+  Camera basicCamera = Camera(basicShader);
 
-    //Creates a window, saves it in hwnd, and shows on screen
-    window = createWindow(instance);
-    ShowWindow(window, show);
+  //Instancing Testing 
+  //Shader basicShader2 = Shader("resources/VertexShader2.txt", "resources/FragmentShader2.txt");
+  //ISprite::Load(basicShader2);
+  //ISprite::drawSprites(textureAnimated);
 
-    Shader basicShader = Shader("resources/VertexShader.txt", "resources/FragmentShader.txt");
-    Shader basicShader2 = Shader("resources/VertexShader2.txt", "resources/FragmentShader2.txt");
-    Shader debugShader = Shader("resources/VertexShaderDebug.txt", "resources/FragmentShaderDebug.txt");
-    Camera basicCamera = Camera(basicShader);
+  //Creates Sprites
+  Sprite smiley = Sprite(basicShader);
+  Sprite excited = Sprite(basicShader);
+  Sprite calm = Sprite(basicShader);
+  Sprite animated = Sprite(basicShader);
+  Sprite player = Sprite(basicShader);
+  pPlayer = &player;
 
-    ISprite::Load(basicShader2);
-
-    //Creates Three Sprites
-    ISprite smiley = ISprite();
-    ISprite excited = ISprite();
-    //Sprite calm = Sprite(basicShader);
-    Sprite animated = Sprite(basicShader);
-	  Sprite player = Sprite(basicShader);
-	  pPlayer = &player;
-
-    Texture textureSmiley = Texture("resources/Smiley1.png");
-    Texture textureExcited = Texture("resources/Smiley2.png");
-    Texture textureCalm = Texture("resources/Smiley3.png");
+	//Sets up textures 
+  Texture textureSmiley = Texture("resources/Smiley1.png");
+  Texture textureExcited = Texture("resources/Smiley2.png");
+  Texture textureCalm = Texture("resources/Smiley3.png");
 	Texture textureAnimated = Texture("resources/ExampleSpriteSheet.png", 3, 64, 64, 1000.0f);
 	Texture texturePlayerIdle = Texture("resources/FinalRunAnimations.png", 4, 64, 64, 100.0f);
 	Texture texturePlayerRun = Texture("resources/FinalRunAnimations.png", 4, 64, 64, 100.0f, 0, 64);
 	pTexturePlayerRun = &texturePlayerRun;
 	pTexturePlayerIdle = &texturePlayerIdle;
 
-    //Saves the textures into the sprites 
-    smiley.texture = textureSmiley;
-    excited.texture = textureExcited;
-    //calm.texture = textureCalm;
+  //Saves the textures into the sprites 
+  smiley.texture = textureSmiley;
+  excited.texture = textureExcited;
+  calm.texture = textureCalm;
 	animated.texture = textureAnimated;
 	player.texture = texturePlayerIdle;
 
-    //Sets inital values of sprites for their respective tests
-    smiley.translation = glm::vec3(1.0f, 0.5f, 0.0f);
-    excited.translation = glm::vec3(-1.4f, -0.75f, 0.0f);
-    //calm.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    smiley.scale = glm::vec3(0.15f, 0.15f, 0.15f);
+  //Sets inital values of sprites for their respective tests
+  smiley.translation = glm::vec3(1.0f, 0.5f, 0.0f);
+  excited.translation = glm::vec3(-1.4f, -0.75f, 0.0f);
+  calm.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+  smiley.scale = glm::vec3(0.15f, 0.15f, 0.15f);
 	animated.translation = glm::vec3(-1.5f, 1.0f, 0.0f);
 
-    basicShader.Use();
+
 	//I know three parts to drawing isnt ideal but this will eventually be in a larger
 	//init graphics function or something. 
 	initDebugDraw(basicShader); 
+  basicShader.Use();
 
-    //This should probably eventually use a game state manager like cs230
-    //But this is fine for now 
-    while (!shouldQuit)
+  //This should probably eventually use a game state manager like cs230
+  //But this is fine for now 
+  while (!shouldQuit)
+  {
+    wglMakeCurrent(deviceContext, renderingContext);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    testColor(calm);
+    testTranslate(calm);
+    testRotation(excited);
+    testScaling(smiley);
+
+    basicCamera.update();
+    Sprite::drawSprites();
+
+	  //Again, not ideal, but there will eventually be a draw all function or something
+	  debugDrawFrame();
+
+	  debugDrawLine(smiley.translation, calm.translation, glm::vec3(1.0f, 1.0f, 1.0f));
+	  debugDrawLine(excited.translation, calm.translation, glm::vec3());
+	  debugDrawLine(animated.translation, calm.translation, glm::vec3());
+
+	  debugDrawSquare(calm.translation, calm.scale.x, calm.scale.y, glm::vec3());
+	  debugDrawSquare(smiley.translation, smiley.scale.x, smiley.scale.y, glm::vec3());
+	  debugDrawSquare(excited.translation, excited.scale.x, excited.scale.y, glm::vec3());
+
+	  debugDrawCircle(calm.translation, calm.scale.x, glm::vec3(), 100);
+	  debugDrawCircle(animated.translation, 0.5*animated.scale.x, glm::vec3(), 100);
+
+    SwapBuffers(deviceContext);
+
+    if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
     {
-
-        wglMakeCurrent(deviceContext, renderingContext);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        basicCamera.update();
-
-
-        ISprite::drawSprites(textureAnimated);
-
-        //Sprite::drawSprites();
-
-		    //Again, not ideal, but there will eventually be a draw all function or something
-		    //debugDrawFrame();
-
-	        //debugDrawLine(smiley.translation, calm.translation, glm::vec3(1.0f, 1.0f, 1.0f));
-		    //debugDrawLine(excited.translation, calm.translation, glm::vec3());
-		    //debugDrawLine(animated.translation, calm.translation, glm::vec3());
-
-		    //debugDrawSquare(calm.translation, calm.scale.x, calm.scale.y, glm::vec3());
-		    debugDrawSquare(smiley.translation, smiley.scale.x, smiley.scale.y, glm::vec3());
-		    debugDrawSquare(excited.translation, excited.scale.x, excited.scale.y, glm::vec3());
-
-		    //debugDrawCircle(calm.translation, calm.scale.x, glm::vec3(), 100);
-	    	debugDrawCircle(animated.translation, 0.5*animated.scale.x, glm::vec3(), 100);
-
-        SwapBuffers(deviceContext);
-
-        if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-
-        wglMakeCurrent(NULL, NULL);
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
     }
 
-    return 0;
+    wglMakeCurrent(NULL, NULL);
+  }
+  return 0;
 }
 
 //Callback function 
@@ -136,7 +137,6 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
   switch (msg)
   {
-	  
 	case WM_KEYDOWN:
 	{
 		switch (wp)
@@ -146,70 +146,69 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 				pPlayer->translation.x = pPlayer->translation.x -= 0.25;
 				pPlayer->scale.x = -1;
 				pPlayer->texture = *pTexturePlayerRun;
+        break;
 			}
-			break;
 			case VK_RIGHT:
 			{
 				pPlayer->translation.x = pPlayer->translation.x += 0.25;
 				pPlayer->scale.x = 1;
 				pPlayer->texture = *pTexturePlayerRun;
+        break;
 			}
-			break;
+
 			default:
 			{
-				pPlayer->scale.x = 1;
 				if (pPlayer->texture.ID != pTexturePlayerIdle->ID)
 					pPlayer->texture = *pTexturePlayerIdle;
+        break;
 			}
 		}
 	}
-
-	break;
+  break;
 	
-    case WM_CREATE:
+  case WM_CREATE:
+  {
+    //Creates a Dummy Context so a real context can be created
+    //Why? I dunno. They made it this way. It's dumb. 
+    deviceContext = GetDC(hWnd);
+    setupPixelFormatDescriptor(deviceContext);
+    HGLRC dummyContext = wglCreateContext(deviceContext);
+    wglMakeCurrent(deviceContext, dummyContext);
+
+    //Initalizes Glew
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+      std::cout << "Failed to initalize GLEW." << std::endl;
+    else
+      std::cout << "Glew Initalized Successfully." << std::endl;
+
+    //Saves attributes of the real context in an array
+    int contextAttributes[] =
     {
-      //Creates a Dummy Context so a real context can be created
-      //Why? I dunno. They made it this way. It's dumb. 
-      deviceContext = GetDC(hWnd);
-      setupPixelFormatDescriptor(deviceContext);
-      HGLRC dummyContext = wglCreateContext(deviceContext);
-      wglMakeCurrent(deviceContext, dummyContext);
-
-      //Initalizes Glew
-      glewExperimental = GL_TRUE;
-      if (glewInit() != GLEW_OK)
-        std::cout << "Failed to initalize GLEW." << std::endl;
-      else
-        std::cout << "Glew Initalized Successfully." << std::endl;
-
-      //Saves attributes of the real context in an array
-      int contextAttributes[] =
-      {
-        WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
-        WGL_CONTEXT_MINOR_VERSION_ARB, 3,
-        WGL_CONTEXT_FLAGS_ARB,
-        WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
-        0
-      };
+      WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+      WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+      WGL_CONTEXT_FLAGS_ARB,
+      WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB, 0
+    };
       
-      //Creates the Real context and activates it 
-      renderingContext = wglCreateContextAttribsARB(deviceContext, 0, contextAttributes);
-      wglMakeCurrent(deviceContext, renderingContext);
+    //Creates the Real context and activates it 
+    renderingContext = wglCreateContextAttribsARB(deviceContext, 0, contextAttributes);
+    wglMakeCurrent(deviceContext, renderingContext);
       
-      //Deletes Dummy Context 
-      wglDeleteContext(dummyContext);
+    //Deletes Dummy Context 
+    wglDeleteContext(dummyContext);
 
-      if (!renderingContext)
-        std::cout << "Failed to Create Rendering Context." << std::endl;
-      else
-        std::cout << "Redering Context Created Successfully." << std::endl;
-    }
-    break;
+    if (!renderingContext)
+      std::cout << "Failed to Create Rendering Context." << std::endl;
+    else
+      std::cout << "Redering Context Created Successfully." << std::endl;
+  }
+  break;
 
-    case WM_DESTROY:
-      shouldQuit = true;
+  case WM_DESTROY:
+    shouldQuit = true;
 
-    default:    
-        return DefWindowProc(hWnd, msg, wp, lp);
+  default:    
+      return DefWindowProc(hWnd, msg, wp, lp);
   }
 }
