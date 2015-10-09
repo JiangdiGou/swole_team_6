8b211b33b90bcd706cadf54953a05d2e804153ea
Qbz23 <conorlavelleqbz@gmail.com>
2015-10-09 15:42:07 -0700

    Batch File Kinda works
    It just needs to be cleaned up a bit. Will do tonight or tommorow. I wouldn't reccomend running it, though nothing that bad should happen

--- Batch Files/LAUNCHME.bat
+++ Batch Files/LAUNCHME.bat
@@ -1,9 +1,24 @@
-@echo off
-@echo Trying to pack sprites into a sheet
-@echo "%cd%"
-@cd "..\GAM200_Project\resources\Textures"
+@REM Gets rid of some of the output
+@ECHO off
+@ECHO Packing textures in source into atlas
+@ECHO 
+set /p outputName=What would you like to name this atlas?
 
-dir /b "..\Textures" > fileList.txt
+@REM Moves from batch dir to texture dir 
+@CD "..\GAM200_Project\resources\Textures"
 
-"spriteSheetPacker/sspack.exe" /image:out.png /il:fileList.txt /r
-pause
+@REM deletes files that this generates if they exist
+IF EXIST textureList.txt  DEL /F textureList.txt
+IF EXIST %outputName% DEL /F  %outputName%
+
+cd "Source"
+
+@REM puts all of the filenames in a text file 
+DIR /b *png > ../textureList.txt
+
+@REM Moves out of the source directory
+echo %outputName%
+
+@REM packs the files into an atas
+"../spriteSheetPacker/sspack.exe" /image:%outputName% /il:textureList.txt /r
+PAUSE
