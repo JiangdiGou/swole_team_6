b567247b80ea51976239a0dc4c322b0958de1427
Nolanlemahn <nolan@projectexist.net>
2015-10-19 16:29:36 -0700

    baseline ini.lua script I'd like to eventually use...

---
+++ GAM200_Project/Scripts/ini.lua
@@ -0,0 +1,13 @@
+--Script support
+titleAdds = { "We have Lua!", "Candy is actually popcorn...", "Unicorn's are illegal?" }
+
+--[gfxConfig]
+windowTitle = "NinjaCade - " .. titleAdds[ math.random( #titleAdds )]
+
+--[renderConfig]
+	--1920x1080 when?
+clientWidth = 1900
+clientHeight = 900
+
+--[debugMode]
+debugMode = true
